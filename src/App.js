66cbcabc89ba70e49fdd334644f0b5a09c319457
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-10-13 18:47:17 +0100

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+const Content = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+const Total = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header {course}/>
+      <Content {}/>
+      <Total {}/>
+    </div>
+  )
+}
+
+
+export default App
