e2f2a1fa92355dcc4d8ea07def84cf9b1eb29ee1
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-10-21 10:12:03 +0100

    feat: props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-const Header = () => {
+const Header = (props) => {
   return (
     <div>
-
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
-const Content = () => {
+const Content = (props) => {
   return (
     <div>
-
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
     </div>
   )
 }
 
-const Total = () => {
+const Total = (props) => {
   return (
     <div>
-      
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
     </div>
   )
 }
@@ -37,9 +45,10 @@ const App = () => {
 
   return (
     <div>
-      <Header {course}/>
-      <Content {}/>
-      <Total {}/>
+      <Header course={course}/>
+      <Content part1 ={part1} part2={part2} part3={part3} 
+      exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
